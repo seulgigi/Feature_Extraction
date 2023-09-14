a1aa5795c815b5014ca759fd95fe318753012510
Seulgigi <2021112414@dgu.ac.kr>
2023-09-14 18:34:50 +0900

    unit dict 형태

--- GPEE.py
+++ GPEE.py
@@ -1,6 +1,14 @@
 from sklearn.model_selection import train_test_split
 from FeatureExtraction import feature_extraction
-def GPFE(data, split_portion: float, ML_model, GP_config: dict, unit: list):
+def GPFE(data, split_portion: float, ML_model, GP_config: dict, unit: dict):
+
+    train_unit = []
+    for feature in unit.keys():
+        if unit[feature] == None:
+            data = data.drop([feature], axis=1)
+        else:
+            train_unit.append(unit[feature])
+
     if split_portion:
         train, test = train_test_split(data, stratify=data['Decision'], test_size=split_portion, random_state=2)
         train, validation = train_test_split(data, stratify=data['Decision'], test_size=split_portion, random_state=2)
@@ -15,7 +23,7 @@ def GPFE(data, split_portion: float, ML_model, GP_config: dict, unit: list):
         train, validation, test = data.to_dict(), data.to_dict(), data.to_dict()
 
     attribute_name = [column for column in data.columns if len(data[column].unique()) > 2] # unique값이 2개 초과 -> encoding 열 제외
-    attribute = dict(zip(attribute_name, unit))
+    attribute = dict(zip(attribute_name, train_unit))
     data = data.to_dict()
 
-    feature_extraction(GP_config, ML_model, attribute, attribute_name, train, validation, test, data, unit)
+    feature_extraction(GP_config, ML_model, attribute, attribute_name, train, validation, test, data, train_unit)
